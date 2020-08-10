84e500983c60707bb2cd8777ec66fc02c1384225
Noah Eakin <noaheakin@gmail.com>
2020-08-10 19:40:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,2 @@
 def greet_characters(array)
-  array.each do |element|
-    puts "Hello #{element}!"
-  end
-end
-
-def list_dwarves(array)
-  array.each_with_index do |element, index|
-    puts "#{index + 1}. #{element}"
-  end
-end
+  
